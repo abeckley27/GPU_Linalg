c36aa51509f97f6f51a9f9881b6a2514dcdf5a07
Aidan Beckley <aidanbeckley@gmail.com>
2023-02-23 13:41:45 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <random>
+#include <omp.h>
+
+void mult(float** A, float** B, float** C) 
+{
+
+}
+
+int main(int argc, char* argv[]) {
+
+    const int N = 1000;
+
+    std::cout << "Size of matrix: " << N << " x " << N << std::endl;
+    std::cout << "Size of float: " << sizeof(float) << std::endl;
+
+    
+
+
+    return 0;
+}

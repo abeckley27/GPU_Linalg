e1220642fd8d87e0f4fb22c228f498bb55d2945d
Aidan <aidanbeckley@gmail.com>
2023-02-23 14:40:30 -0500

    Added matrix initialization and function declarations in header

--- main.cpp
+++ main.cpp
@@ -1,21 +1,49 @@
 #include <iostream>
 #include <random>
 #include <omp.h>
-
-void mult(float** A, float** B, float** C) 
-{
-
-}
+#include "matrix.h"
 
 int main(int argc, char* argv[]) {
 
-    const int N = 1000;
+    const int N = 10;
 
     std::cout << "Size of matrix: " << N << " x " << N << std::endl;
     std::cout << "Size of float: " << sizeof(float) << std::endl;
 
+    std::random_device rd;
+	std::mt19937 gen(rd());
+	std::uniform_real_distribution<> dist(0.0, 10.0);
+
+    int i, j;
+    float** A = new float*[N];
+    float** B = new float*[N];
+    float** C = new float*[N];
+
+    for (i = 0; i < N; i++) 
+    { 
+        A[i] = new float[N];
+        B[i] = new float[N];
+        C[i] = new float[N];
+    }
+
+    for (i = 0; i < N; i++) 
+    {
+        for (j = 0; j < N; j++) 
+        {
+            A[i][j] = dist(gen);
+            B[i][j] = dist(gen);
+        }
+    }
+
+    add(A, B, C);
     
 
+    for (i = 0; i < N; i++) 
+    { 
+        delete[] A[i];
+        delete[] B[i];
+        delete[] C[i];
+    }
 
     return 0;
 }
---
+++ matrix.h
@@ -0,0 +1,20 @@
+#ifndef matrix_h
+#define matrix_h
+
+void print(float** A) 
+{
+    
+}
+
+void add(float** A, float** B, float** C) 
+{
+
+}
+
+void mult(float** A, float** B, float** C) 
+{
+
+}
+
+
+#endif
